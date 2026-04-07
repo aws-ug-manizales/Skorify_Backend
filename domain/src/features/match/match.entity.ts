4ce1bd7b318cc14c6d3d268f4a6cfa7b60adc5f7
Bryan Arroyave Ortiz <barroyave@deuna.com>
2026-04-07 11:55:15 -0500

    Convierte entidades de interfaces a clases con constructores y herencia de Entity base

--- domain/src/features/match/match.entity.ts
+++ domain/src/features/match/match.entity.ts
@@ -1,18 +1,20 @@
 import { Entity, Id } from "../../core/entity";
 
-export class MatchEntity implements Entity {
-  id: Id;
+export class MatchEntity extends Entity {
   awayTeamId: string;
   localTeamId: string;
   date: Date;
 
-  constructor(id: Id, awayTeamId: string, localTeamId: string, date: Date) {
-    this.id = id;
+  private constructor(id: Id, awayTeamId: string, localTeamId: string, date: Date) {
+    super(id);
     this.awayTeamId = awayTeamId;
     this.localTeamId = localTeamId;
     this.date = date;
   }
 
+  build(params: { awayTeamId: string; localTeamId: string; date: Date }): MatchEntity {
+    return new MatchEntity(this.id, params.awayTeamId, params.localTeamId, params.date);
+  }
 
   public canBet(): boolean {
     // TODO: Implement business logic to determine if betting is allowed
