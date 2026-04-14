3e04f0417bd0d5745bfb2094835e8c98652303c1
DanielVargasG <daniel.vargon01@gmail.com>
2026-04-13 23:12:54 -0500

    Refactorización userContract para que herede del BaseContract

--- domain/src/features/user/user.contract.ts
+++ domain/src/features/user/user.contract.ts
@@ -1,6 +1,5 @@
 import { UserEntity } from "./user.entity";
+import { BaseContract } from "../../core";
 
-export abstract class UserContract {
-  abstract getById(id: string): Promise<UserEntity | null>;
-  abstract save(user: UserEntity): Promise<UserEntity | null>;
+export abstract class UserContract extends BaseContract<UserEntity> {
 }
