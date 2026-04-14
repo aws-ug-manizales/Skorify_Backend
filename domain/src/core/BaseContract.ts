f736ff73d1fca0e8b188db8b7559fcc8a7f9ddf9
DanielVargasG <daniel.vargon01@gmail.com>
2026-04-13 20:35:22 -0500

    Se crea el contratoBase dentro del core

---
+++ domain/src/core/BaseContract.ts
@@ -0,0 +1,13 @@
+import { Entity } from "./entity";
+
+// BaseContract es una clase abstracta que define los métodos básicos para manejar entidades en el dominio.
+// Recibe un tipo genérico T que extiende de Entity, lo que permite que cualquier clase que implemente BaseContract pueda trabajar con cualquier tipo de entidad que tenga un id.
+export abstract class BaseContract<T extends Entity> {
+  abstract getById(id: string): Promise<T | null>;
+  abstract save(entity: T): Promise<T  | null>;
+  abstract deleteById(id: string): Promise<T | null>;
+  abstract modifyById(id: string, entity: T): Promise<T | null>;
+  abstract getAll(): Promise<T[]>;
+  abstract getByIDs(ids: string[]): Promise<T[]>;
+  abstract filter(filters: T[]): Promise<T[]>;
+}
