abb95744fb95551121e1f6d64dc64380d67d8228
DanielVargasG <daniel.vargon01@gmail.com>
2026-04-16 21:01:24 -0500

    Refactor nombre archivo contrato base

--- domain/src/core/index.ts
+++ domain/src/core/index.ts
@@ -1,3 +1,3 @@
 export { DomainEvent, DomainEventKind } from "./domain-event";
-export { BaseContract } from "./BaseContract";
+export { BaseContract } from "./base.contract";
 export { generalMethodMapper } from "./method.mapper";
