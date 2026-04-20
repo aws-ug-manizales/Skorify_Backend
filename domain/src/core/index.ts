940ad4e467ce921ce28e80bf1fa455cb652c789e
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2026-04-19 20:58:44 -0500

    Actualiza builders

--- domain/src/core/index.ts
+++ domain/src/core/index.ts
@@ -1,3 +1,4 @@
 export { DomainEvent, DomainEventKind } from "./domain-event";
 export { BaseContract } from "./BaseContract";
+export { Entity, Id } from "./entity";
 export { generalMethodMapper } from "./method.mapper";
