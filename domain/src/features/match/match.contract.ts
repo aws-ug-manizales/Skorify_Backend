755bb5039e813c79dee060ea8ca5ad1feecee2c3
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2026-04-24 00:18:43 -0500

    Update

--- domain/src/features/match/match.contract.ts
+++ domain/src/features/match/match.contract.ts
@@ -1,6 +1,6 @@
 import { MatchEntity } from "./match.entity";
 import { PredictionEntity } from "../prediction/prediction.entity";
-import { BaseContract } from "@/core";
+import { BaseContract } from "../../core";
 
 export abstract class MatchContract extends BaseContract<MatchEntity> {
   abstract getPredictionsByMatchId(matchId: string): Promise<PredictionEntity[] | null>;
