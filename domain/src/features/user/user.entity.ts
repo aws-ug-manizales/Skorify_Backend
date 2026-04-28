9f43018f408cf9bddbab196129ddaffb7c8f6d8a
Daniell-Estrada <nelson.1702013546@ucaldas.edu.co>
2026-04-27 22:03:46 -0500

    feat(user): implement registerNotificationToken usecase
    - Add optional notificationToken attribute to UserEntity
    - Create RegisterNotificationTokenUsecase and param interface
    - Add NotificationTokenAssignedDomainEvent and NotificationTokenAssignmentFailedDomainEvent
    - Implement RegisterNotificationTokenUsecaseImpl in server
    - Expose HTTP endpoint: PUT /user/register-notification-token
    - Fix invalid main and types paths in shared package.json

--- domain/src/features/user/user.entity.ts
+++ domain/src/features/user/user.entity.ts
@@ -3,15 +3,18 @@ import { Entity, Id } from "../../core/entity";
 export interface UserAttributes {
   id: Id;
   name: string;
+  notificationToken: string;
 }
 
 export class UserEntity extends Entity {
   name: string;
+  notificationToken: string;
 
   private constructor(attributes: UserAttributes) {
-    const { id, name } = attributes;
+    const { id, name, notificationToken } = attributes;
     super(id);
     this.name = name;
+    this.notificationToken = notificationToken;
   }
 
   static build(params: UserAttributes): UserEntity {
