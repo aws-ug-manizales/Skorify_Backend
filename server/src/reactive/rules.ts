7f4781b5fc59533f7d678b2bf40e83b76f82ad27
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2026-05-06 21:22:39 -0500

    Update

--- server/src/reactive/rules.ts
+++ server/src/reactive/rules.ts
@@ -1,10 +1,18 @@
 import { EventBusContract } from "@skorify/domain/core";
 import {
+  CalculateMatchScoreUsecase,
   GottenMatchDomainEvent,
   ReactiveClosedMatchDomainEvent,
 } from "@skorify/domain/match";
 
 export function configureRules(eventBusContract: EventBusContract) {
+
+
+
+  eventBusContract.on(ReactiveClosedMatchDomainEvent,CalculateMatchScoreUsecase);
+  
+  eventBusContract.on(ReactiveClosedMatchDomainEvent,CalculateMatchScoreUsecase);
+
   eventBusContract.afterGroupCompletion({
     domainEvent: ReactiveClosedMatchDomainEvent,
     completionHandler: (event) => {
