aa1cf43769632bbc828ee464522fa215cfd953a5
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2026-06-03 17:23:08 -0500

    Update

--- server/src/features/match/usecases/get-matches-by-tournament-id.usecase-impl.ts
+++ server/src/features/match/usecases/get-matches-by-tournament-id.usecase-impl.ts
@@ -4,22 +4,39 @@ import {
   GetMatchesByTournamentIdUsecase,
   GottenMatchesByTournamentDomainEvent,
   MatchContract,
+  MatchEntity,
 } from '@skorify/domain/match';
 
+import { GetTeamByIdsUsecase, TeamEntity } from '@skorify/domain/team';
+
 export class GetMatchesByTournamentIdUsecaseImpl extends GetMatchesByTournamentIdUsecase {
-  constructor(private matchContract: MatchContract) {
+  constructor(
+    private matchContract: MatchContract,
+    private getTeamByIdsUsecase: GetTeamByIdsUsecase,
+  ) {
     super();
   }
 
   async call(param: GetMatchesByTournamentIdParam): Promise<DomainEvent> {
     const { tournamentId } = param;
-    console.log({tournamentId});
 
-    const matches = await this.matchContract.filter({
+    const matches: MatchEntity[] = await this.matchContract.filter({
       where: {
         tournamentId,
       },
     });
-    return GottenMatchesByTournamentDomainEvent(matches);
+
+    const teamIds = matches.map((match) => [match.teamAId, match.teamBId]).flat();
+    const uniqueTeamIds = [...new Set(teamIds)];
+
+    const teamsDE = await this.getTeamByIdsUsecase.call({ teamIds: uniqueTeamIds });
+
+    const teams: TeamEntity[] = teamsDE.payload;
+    const response = matches.map((match) => {
+      const awayTeam = teams.find((team) => team.id === match.awayTeamId)!;
+      const homeTeam = teams.find((team) => team.id === match.homeTeamId)!;
+      return { match, awayTeam, homeTeam };
+    });
+    return GottenMatchesByTournamentDomainEvent(response);
   }
 }
